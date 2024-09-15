8148e719fa924ee17d5f9887dbdb29c87ce624e9
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-09-15 20:42:46 +0530

    solving error in faq

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://fruit-ai-1cw1.vercel.app/",
+    origin: "https://fruit-ai-1cw1.vercel.app",
 }));
 
 const storage = multer.diskStorage({
